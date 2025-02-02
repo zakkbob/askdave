f087ed186522a9a70323d6262ecb956312b55e43
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-02-02 15:57:45 +0000

    feat: dodgy code

--- crawler/main.go
+++ crawler/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+)
+
+func fetch_page(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func extract_page_info(page string) (page_title, og_title, og_description string) {
+	title_regex, _ := regexp.Compile("(?s)property=\"og:title\".*?content=\"(.*?)\"")
+	matches := title_regex.FindStringSubmatch(page)
+	page_title = matches[0]
+
+	og_title = ""
+	og_description = ""
+
+	return
+}
+
 func main() {
-	
+	url := "https://greendungarees.org.uk"
+	body, _ := fetch_page(url)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Printf("%s", body)
+	// }
+	page_title, _, _ := extract_page_info(body)
+	fmt.Println(page_title)
 }
