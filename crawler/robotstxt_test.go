0c636c0eeb016da0306877140767a3b622ae943f
Zakk <zakkcarpenter@hotmail.com>
2025-02-08 16:43:55 +0000

    style: run gofmt

--- crawler/robotstxt_test.go
+++ crawler/robotstxt_test.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"testing"
 	"os"
+	"testing"
 )
 
 func getRobotsTxt(t *testing.T) string {
 	content, err := os.ReadFile("testdata/robots.txt")
-    if err != nil {
-        t.Fatalf("Failed to read file: %v", err)
-    }
-    return string(content)
+	if err != nil {
+		t.Fatalf("Failed to read file: %v", err)
+	}
+	return string(content)
 }
 
 func TestUrlValidator(t *testing.T) {
-    robotsTxt := getRobotsTxt(t)
+	robotsTxt := getRobotsTxt(t)
 
 	validator, _ := ProcessRobotsTxt(robotsTxt)
 
 	testUrls := map[string]bool{
-		"/allowed-dir": true,
-		"/disallowed" : false,
-		"/disallowed/subdir": false,
-		"/disallowed/nevermind-this-is-allowed": true,
+		"/allowed-dir":                              true,
+		"/disallowed":                               false,
+		"/disallowed/subdir":                        false,
+		"/disallowed/nevermind-this-is-allowed":     true,
 		"/disallowed/nevermind-this-is-allowed/lol": true,
-		"/main.php": false,
-		"/endline": false,
-		"/endline/not-lol": true,
-		"/allowed-dir/disallowed-php.php": false,
-		"/allow-subdir/": false,
-		"/allow-subdir/e": true,
+		"/main.php":                                 false,
+		"/endline":                                  false,
+		"/endline/not-lol":                          true,
+		"/allowed-dir/disallowed-php.php":           false,
+		"/allow-subdir/":                            false,
+		"/allow-subdir/e":                           true,
 	}
 
 	for url, want := range testUrls {
