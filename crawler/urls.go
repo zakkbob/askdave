357126fa222b90945c4b9d153c1bc838b40d6212
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-02-08 18:55:04 +0000

    feat: add port to url struct

--- crawler/urls.go
+++ crawler/urls.go
@@ -1,12 +1,15 @@
 package main
 
-//import "fmt"
+import (
+	"strconv"
+)
 
 type url struct {
 	protocol      string
 	subdomain     string
 	domain        string
 	tld           string
+	port          int
 	path          []string
 	trailingSlash bool
 }
@@ -24,6 +27,10 @@ func (u *url) String() string {
 
 	s += u.domain + "."
 	s += u.tld
+
+	if (u.port != 0) && !(u.protocol == "http" && u.port == 80) && !(u.protocol == "https" && u.port == 443) {
+		s += ":" + strconv.Itoa(u.port)
+	}
 
 	if len(u.path) != 0 {
 		for _, p := range u.path {
