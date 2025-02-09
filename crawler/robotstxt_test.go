19dcc5f4d2d97df81cf803737a3121c5fcff4969
ZakkBob <zakkcarpenter@hotmail.com>
2025-02-09 14:31:29 +0000

    chore: added test for ExtractDavebotDirectives

--- crawler/robotstxt_test.go
+++ crawler/robotstxt_test.go
@@ -5,16 +5,32 @@ import (
 	"testing"
 )
 
-func getRobotsTxt(t *testing.T) string {
-	content, err := os.ReadFile("testdata/robots.txt")
+func getRobotsTxt(t *testing.T, fileName string) string {
+	content, err := os.ReadFile("testdata/" + fileName)
 	if err != nil {
 		t.Fatalf("Failed to read file: %v", err)
 	}
 	return string(content)
 }
 
+func TestExtractDavebotDirectives(t *testing.T) {
+	blocks := map[string]string{
+		"*": "Disallow: /",
+		"davebot/0.1": "Allow: /Davebot0.1/",
+		"davebot": "Allow: /Davebot/",
+		"bingbot": "Disallow: /bingbot/",
+	}
+	
+	got := extractDavebotDirectives(blocks)
+	want := "Allow: /Davebot0.1/"
+
+	if got != want {
+		t.Errorf("got %s, wanted %s", got, want)
+	}
+}
+
 func TestUrlValidator(t *testing.T) {
-	robotsTxt := getRobotsTxt(t)
+	robotsTxt := getRobotsTxt(t, "robots.txt")
 
 	validator, _ := ProcessRobotsTxt(robotsTxt)
 
@@ -35,7 +51,7 @@ func TestUrlValidator(t *testing.T) {
 	for url, want := range testUrls {
 		got := validator.validate(url)
 		if got != want {
-			t.Errorf("'%s' got %t, want %t", url, got, want)
+			t.Errorf("'%s' got %t, wanted %t", url, got, want)
 		}
 	}
 }
