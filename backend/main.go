ba1b7bb02c9258109c8fb309ed2d63689e0cfb96
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-02-18 18:26:04 +0000

    dodginess

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "github.com/ZakkBob/AskDave/gocommon/url"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+var db *sql.DB
 
 func main() {
-	_ = url.Url{}
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:password@127.0.0.1:5432/recordings")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var title string
+	var price float64
+	err = conn.QueryRow(context.Background(), "select title, price from album where id=$1", 1).Scan(&title, &price)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(title, price)
 }
