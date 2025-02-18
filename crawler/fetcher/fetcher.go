e1db183ce7557a5351dc4d09faddded1515999d3
ZakkBob <114868973+ZakkBob@users.noreply.github.com>
2025-02-18 12:56:07 +0000

    Crawl Request Features (#49) :D
    * im guessing my dogy git commands worked :D
    
    * chore(crawler, backend): fix formatting
    
    * redeclare main real quick to make actions happy again
    
    * chore(crawler, backend): fix formatting
    
    * i should probably sleep
    
    * chore(crawler, backend): fix formatting
    
    * im slowly developing a mild hatred for html
    
    * chore(crawler, backend): fix formatting
    
    * little checkpoint cuz im gonna rewrite it because I need to do robots.txt first
    
    * feat: add uhhmm, stuff. ig?
    
    * feat: add tasks struct
    
    * feat: add fetchers
    
    * Working on segmenting the file structure [no ci]
    
    * feat: refactor stuff (i need to support ips)
    
    * fix: run all tests
    
    * feat: add hash (needs proper test)
    
    * feat: integrate TaskRunner with Fetcher
    
    * feat: add hash abstraction, make all test files a seperate package, other stuff probs
    
    * feat: add hash_test
    
    * add Marshal and Unmarshal for url and hash
    
    * fix: fix url unmarshal, add tests
    
    * feat: add json struct tags
    
    * need to debug
    
    * move testdata, use embed, ignore query strings, idk
    
    * feat: add fetcher.Response
    
    * feat: indicate success in results
    
    * need to fix url.Copy() [no ci]
    
    * fix(urls): don't escape end-line symbol in regex ($)
    
    * fix: url.Copy() copies path correctly
    
    * task runner works now i think
    
    * add delay option to file fetcher
    
    * feat: it works, but the code definitely needs a good refactor. On to the frontend for now
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ crawler/fetcher/fetcher.go
@@ -0,0 +1,140 @@
+//------------------------------------------------------//
+// An interface for fetching webpages, and some structs //
+//------------------------------------------------------//
+
+package fetcher
+
+import (
+	"ZakkBob/AskDave/crawler/url"
+	"embed"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+//go:embed testsites/*
+var files embed.FS
+
+type Fetcher interface {
+	Fetch(string) (Response, error)
+}
+
+type Response struct {
+	Body       string
+	StatusCode int
+}
+
+type NetFetcher struct {
+	Debug bool
+}
+
+func (f *NetFetcher) Fetch(u string) (Response, error) {
+	resp, err := http.Get(u)
+	if f.Debug {
+		fmt.Printf("fetching url '%s'\n", u)
+	}
+	if err != nil {
+		return Response{}, err // Get was unsuccessful, url probably doesnt exist or something, who knows
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Response{}, err
+	}
+	if f.Debug {
+		fmt.Printf("fetched url '%s'\n", u)
+	}
+	return Response{
+		Body:       string(body),
+		StatusCode: resp.StatusCode,
+	}, nil
+}
+
+type DummyFetcher struct {
+	Response  string
+	Delay     time.Duration
+	RandDelay time.Duration
+	Debug     bool
+}
+
+func (f *DummyFetcher) sleep() time.Duration {
+	if f.Delay == 0 && f.RandDelay == 0 {
+		return 0
+	}
+
+	if f.RandDelay == 0 {
+		time.Sleep(f.Delay)
+		return f.Delay
+	}
+
+	extraDelay := time.Duration(rand.Int() % (int(f.RandDelay)))
+	totalDelay := f.Delay + extraDelay
+	time.Sleep(totalDelay)
+	return totalDelay
+}
+
+// returns DummyFetcher.response, the url parameter is ignored
+func (f *DummyFetcher) Fetch(u string) (Response, error) {
+	totalDelay := f.sleep()
+
+	if f.Debug {
+		fmt.Printf("fetched dummy url '%s' after %s\n", u, totalDelay.String())
+	}
+	return Response{
+		Body:       f.Response,
+		StatusCode: 200,
+	}, nil
+}
+
+type FileFetcher struct {
+	Delay     time.Duration
+	RandDelay time.Duration
+	Debug     bool
+}
+
+func (f *FileFetcher) sleep() time.Duration {
+	if f.Delay == 0 && f.RandDelay == 0 {
+		return 0
+	}
+
+	if f.RandDelay == 0 {
+		time.Sleep(f.Delay)
+		return f.Delay
+	}
+
+	extraDelay := time.Duration(rand.Int() % (int(f.RandDelay)))
+	totalDelay := f.Delay + extraDelay
+	time.Sleep(totalDelay)
+	return totalDelay
+}
+
+func (f *FileFetcher) Fetch(s string) (Response, error) {
+	totalDelay := f.sleep()
+
+	u, err := url.ParseAbs(s)
+	if err != nil {
+		return Response{}, fmt.Errorf("fetching file: %v", err)
+	}
+
+	path := "testsites/"
+	path += u.FQDN() + u.PathString()
+	content, err := files.ReadFile(path)
+	if f.Debug {
+		fmt.Printf("fetching file url '%s' after %s\n", u.String(), totalDelay.String())
+		fmt.Printf("path: '%s'\n", path)
+		// fmt.Printf("content: '%s'\n", content)
+	}
+	if err != nil {
+		fmt.Printf("fetching file url '%s': %v\n", u.String(), err)
+		return Response{
+			Body:       "",
+			StatusCode: 404,
+		}, nil
+	}
+	return Response{
+		Body:       string(content),
+		StatusCode: 200,
+	}, nil
+}
