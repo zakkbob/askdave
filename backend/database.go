dc204db44ad2c97fcb4b0606076169d2f4311d33
Zakk <zakkcarpenter@hotmail.com>
2025-02-20 00:42:44 +0000

    works, i hope

--- backend/database.go
+++ backend/database.go
@@ -10,6 +10,90 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
+func saveResults(r *tasks.Results) error {
+	robotsQuery := `UPDATE robots 
+		SET (allowed_patterns, disallowed_patterns, last_crawl) = ($1, $2, CURRENT_DATE) 
+		FROM site WHERE site.url = $3 AND site.id = robots.site_id;`
+
+	for urlS, robotsResult := range r.Robots {
+		if !robotsResult.Changed {
+			continue
+		}
+
+		if !robotsResult.Success {
+			continue // temporary - should probably do something
+		}
+
+		_, err := dbpool.Exec(context.Background(), robotsQuery, robotsResult.Validator.AllowedPatterns, robotsResult.Validator.DisallowedPatterns, urlS)
+		if err != nil {
+			return fmt.Errorf("unable to save robots result: %v", err)
+		}
+	}
+
+	for urlS, pageResult := range r.Pages {
+		if !pageResult.Changed {
+			continue
+		}
+
+		// Add crawl to db
+		if pageResult.Success {
+			if pageResult.Changed {
+				query := `INSERT INTO crawl (page_id, datetime, success, content_changed, title, og_title, og_description, hash)
+					SELECT page.id, CURRENT_TIMESTAMP, TRUE, TRUE, $1, $2, $3, $4 FROM page WHERE url = $5;`
+
+				_, err := dbpool.Exec(context.Background(), query, pageResult.Page.Title, pageResult.Page.OgTitle, pageResult.Page.OgDescription, pageResult.Page.Hash)
+				if err != nil {
+					return fmt.Errorf("unable to save page result: %v", err)
+				}
+			} else {
+				query := `INSERT INTO crawl (page_id, datetime, success, content_changed)
+					SELECT page.id, CURRENT_TIMESTAMP, TRUE, FALSE FROM page WHERE url = $1;`
+
+				_, err := dbpool.Exec(context.Background(), query, urlS)
+				if err != nil {
+					return fmt.Errorf("unable to save page result: %v", err)
+				}
+			}
+		} else {
+			query := `INSERT INTO crawl (page_id, datetime, success)
+				SELECT page.id, CURRENT_TIMESTAMP, FALSE FROM page WHERE url = $1;`
+
+			_, err := dbpool.Exec(context.Background(), query, urlS)
+
+			if err != nil {
+				return fmt.Errorf("unable to save page result: %v", err)
+			}
+		}
+
+		// Set next crawl date
+		const maxCrawlInterval = 30
+		const minCrawlInterval = 1
+
+		var query string
+
+		if pageResult.Changed {
+			query = `UPDATE page SET 
+				interval_delta = LEAST(interval_delta - 1, -1), 
+				crawl_interval = LEAST(GREATEST(crawl_interval + interval_delta, $1),$2), 
+				next_crawl = next_crawl + interval '1' day * crawl_interval 
+				WHERE url = $3;`
+		} else {
+			query = `UPDATE page SET 
+				interval_delta = GREATEST(interval_delta + 1, 1), 
+				crawl_interval = LEAST(GREATEST(crawl_interval + interval_delta, $1),$2), 
+				next_crawl = next_crawl + interval '1' day * crawl_interval 
+				WHERE url = $3;`
+		}
+
+		_, err := dbpool.Exec(context.Background(), query, maxCrawlInterval, minCrawlInterval, urlS)
+		if err != nil {
+			return fmt.Errorf("unable to update page crawl scheduling: %v", err)
+		}
+	}
+
+	return nil
+}
+
 func nextTasks(n int) (*tasks.Tasks, error) {
 	query := `WITH ordered_crawls AS (
 		SELECT page.id, page.url, page.next_crawl, robots.allowed_patterns, robots.disallowed_patterns,
--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,10 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/ZakkBob/AskDave/gocommon/hash"
+	"github.com/ZakkBob/AskDave/gocommon/robots"
+	"github.com/ZakkBob/AskDave/gocommon/tasks"
+	"github.com/ZakkBob/AskDave/gocommon/url"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -20,14 +24,131 @@ func main() {
 	}
 	defer dbpool.Close()
 
-	t, err := nextTasks(2)
+	// t, err := nextTasks(2)
 
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "%v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	// data, err := json.MarshalIndent(t, "", "  ")
+
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Unable to serialise tasks: %v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println(string(data))
+
+	r := tasks.Results{
+		Robots: make(map[string]*tasks.RobotsResult),
 	}
 
-	data, err := json.MarshalIndent(t, "", "  ")
+	mateiValidator, _ := robots.Parse(`
+#
+# robots.txt
+#
+# This file is to prevent the crawling and indexing of certain parts
+# of your site by web crawlers and spiders run by sites like Yahoo!
+# and Google. By telling these "robots" where not to go on your site,
+# you save bandwidth and server resources.
+#
+# This file will be ignored unless it is at the root of your host:
+# Used:    http://example.com/robots.txt
+# Ignored: http://example.com/site/robots.txt
+#
+# For more information about the robots.txt standard, see:
+# http://www.robotstxt.org/robotstxt.html
+
+User-agent: *
+Crawl-delay: 10
+# CSS, JS, Images
+Allow: /misc/*.css$
+Allow: /misc/*.css?
+Allow: /misc/*.js$
+Allow: /misc/*.js?
+Allow: /misc/*.gif
+Allow: /misc/*.jpg
+Allow: /misc/*.jpeg
+Allow: /misc/*.png
+Allow: /modules/*.css$
+Allow: /modules/*.css?
+Allow: /modules/*.js$
+Allow: /modules/*.js?
+Allow: /modules/*.gif
+Allow: /modules/*.jpg
+Allow: /modules/*.jpeg
+Allow: /modules/*.png
+Allow: /profiles/*.css$
+Allow: /profiles/*.css?
+Allow: /profiles/*.js$
+Allow: /profiles/*.js?
+Allow: /profiles/*.gif
+Allow: /profiles/*.jpg
+Allow: /profiles/*.jpeg
+Allow: /profiles/*.png
+Allow: /themes/*.css$
+Allow: /themes/*.css?
+Allow: /themes/*.js$
+Allow: /themes/*.js?
+Allow: /themes/*.gif
+Allow: /themes/*.jpg
+Allow: /themes/*.jpeg
+Allow: /themes/*.png
+# Directories
+Disallow: /includes/
+Disallow: /misc/
+Disallow: /modules/
+Disallow: /profiles/
+Disallow: /scripts/
+Disallow: /themes/
+# Files
+Disallow: /CHANGELOG.txt
+Disallow: /cron.php
+Disallow: /INSTALL.mysql.txt
+Disallow: /INSTALL.pgsql.txt
+Disallow: /INSTALL.sqlite.txt
+Disallow: /install.php
+Disallow: /INSTALL.txt
+Disallow: /LICENSE.txt
+Disallow: /MAINTAINERS.txt
+Disallow: /update.php
+Disallow: /UPGRADE.txt
+Disallow: /xmlrpc.php
+# Paths (clean URLs)
+Disallow: /admin/
+Disallow: /comment/reply/
+Disallow: /filter/tips/
+Disallow: /node/add/
+Disallow: /search/
+Disallow: /user/register/
+Disallow: /user/password/
+Disallow: /user/login/
+Disallow: /user/logout/
+# Paths (no clean URLs)
+Disallow: /?q=admin/
+Disallow: /?q=comment/reply/
+Disallow: /?q=filter/tips/
+Disallow: /?q=node/add/
+Disallow: /?q=search/
+Disallow: /?q=user/password/
+Disallow: /?q=user/register/
+Disallow: /?q=user/login/
+Disallow: /?q=user/logout/`)
+
+	u, _ := url.ParseAbs("https://mateishome.page")
+	matei := tasks.RobotsResult{
+		Url:           &u,
+		Success:       true,
+		FailureReason: tasks.NoFailure,
+		Changed:       true,
+		Hash:          hash.Hash{},
+		Validator:     &mateiValidator,
+	}
+
+	r.Robots["https://mateishome.page"] = &matei
+
+	data, err := json.MarshalIndent(r, "", "  ")
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to serialise tasks: %v\n", err)
@@ -35,4 +156,16 @@ func main() {
 	}
 
 	fmt.Println(string(data))
+
+	err = saveResults(&r)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
+
+// 1 | https://mateishome.page
+// 2 | https://games.zakkcarpenter.com
+// 3 | https://greendungarees.org.uk
+// 4 | https://zakkcarpenter.com
