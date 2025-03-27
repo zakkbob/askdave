1d3b892ba91894fbc58fa23627b53537698c1747
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-03-27 22:53:46 +0000

    feat: add gocommon.url tests and functions

--- gocommon/url/url_test.go
+++ gocommon/url/url_test.go
@@ -3,134 +3,113 @@ package url_test
 import (
 	"testing"
 
+	"errors"
+
 	"github.com/ZakkBob/AskDave/gocommon/url"
 	"github.com/stretchr/testify/require"
 )
 
+type parseTest struct {
+	in          string
+	out         string
+	parseErr    error
+	parseAbsErr error
+}
+
+var parseTests = []parseTest{
+	{
+		in:          "https://www.example.com",
+		out:         "https://www.example.com",
+		parseErr:    nil,
+		parseAbsErr: nil,
+	},
+	{
+		in:          "http://www.example.com",
+		out:         "http://www.example.com",
+		parseErr:    nil,
+		parseAbsErr: nil,
+	},
+	{
+		in:          "https://www.example.com#fragment",
+		out:         "https://www.example.com",
+		parseAbsErr: nil,
+		parseErr:    nil,
+	},
+	{
+		in:          "https://user_name:password@www.example.com",
+		out:         "https://www.example.com",
+		parseErr:    nil,
+		parseAbsErr: nil,
+	},
+	{
+		in:          "ftp://www.example.com",
+		out:         "",
+		parseErr:    url.ErrInvalidScheme,
+		parseAbsErr: url.ErrInvalidScheme,
+	},
+	{
+		in:          "www.example.com",
+		out:         "www.example.com",
+		parseErr:    nil,
+		parseAbsErr: nil,
+	},
+	{
+		in:          "/example",
+		out:         "/example",
+		parseErr:    nil,
+		parseAbsErr: url.ErrNotAbsolute,
+	},
+}
+
 func TestParse(t *testing.T) {
-	parseTests := []struct {
-		name string
-		in   string
-		out  string
-		err  error
-	}{
-		{
-			name: "https scheme",
-			in:   "https://www.example.com",
-			out:  "https://www.example.com",
-			err:  nil,
-		},
-		{
-			name: "http scheme",
-			in:   "http://www.example.com",
-			out:  "http://www.example.com",
-			err:  nil,
-		},
-		{
-			name: "fragment",
-			in:   "https://www.example.com#fragment",
-			out:  "https://www.example.com",
-			err:  nil,
-		},
-		{
-			name: "credentials",
-			in:   "https://user_name:password@www.example.com",
-			out:  "https://www.example.com",
-			err:  nil,
-		},
-		{
-			name: "invalid scheme",
-			in:   "ftp://www.example.com",
-			out:  "",
-			err:  url.ErrInvalidScheme,
-		},
-		{
-			name: "no scheme",
-			in:   "www.example.com",
-			out:  "www.example.com",
-			err:  nil,
-		},
-		{
-			name: "path",
-			in:   "/example",
-			out:  "/example",
-			err:  nil,
-		},
-	}
+	for _, test := range parseTests {
+		u, err := url.Parse(test.in)
 
-	for _, pt := range parseTests {
-		t.Run(pt.name, func(t *testing.T) {
-			u, err := url.Parse(pt.in)
+		if !errors.Is(err, test.parseAbsErr) {
+			t.Fatalf("expected '%v' error with url %s", test.parseAbsErr, test.in)
+		}
 
-			if pt.err != nil {
-				require.ErrorIs(t, err, pt.err, "Parse should return an error")
-				return
-			} else {
-				require.NoError(t, err, "Parse should not return an error")
-			}
+		if err != nil {
+			return
+		}
 
-			require.Equal(t, pt.out, u.String())
-		})
+		require.Equal(t, test.out, u.String())
 	}
 }
 
 func TestParseAbs(t *testing.T) {
-	parseTests := []struct {
-		name string
-		in   string
-		out  string
-		err  error
-	}{
-		{
-			name: "https scheme",
-			in:   "https://www.example.com",
-			out:  "https://www.example.com",
-			err:  nil,
-		},
-		{
-			name: "http scheme",
-			in:   "http://www.example.com",
-			out:  "http://www.example.com",
-			err:  nil,
-		},
-		{
-			name: "fragment",
-			in:   "https://www.example.com#fragment",
-			out:  "https://www.example.com",
-			err:  nil,
-		},
-		{
-			name: "credentials",
-			in:   "https://user_name:password@www.example.com",
-			out:  "https://www.example.com",
-			err:  nil,
-		},
-		{
-			name: "invalid scheme",
-			in:   "ftp://www.example.com",
-			out:  "",
-			err:  url.ErrInvalidScheme,
-		},
-		{
-			name: "not absolute",
-			in:   "www.example.com",
-			out:  "",
-			err:  url.ErrNotAbsolute,
-		},
+	for _, test := range parseTests {
+		u, err := url.ParseAbs(test.in)
+
+		if !errors.Is(err, test.parseAbsErr) {
+			t.Fatalf("expected '%v' error with url %s", test.parseAbsErr, test.in)
+		}
+
+		if err != nil {
+			return
+		}
+
+		require.Equal(t, test.out, u.String())
 	}
+}
 
-	for _, pt := range parseTests {
-		t.Run(pt.name, func(t *testing.T) {
-			u, err := url.ParseAbs(pt.in)
+type stringNoPathTest struct {
+	in  string
+	out string
+}
 
-			if pt.err != nil {
-				require.ErrorIs(t, err, pt.err, "Parse should return an error")
-				return
-			} else {
-				require.NoError(t, err, "Parse should not return an error")
-			}
+var stringNoPathTests = []stringNoPathTest{
+	{
+		in:  "https://example.com/path/example/test/",
+		out: "https://example.com",
+	},
+}
 
-			require.Equal(t, pt.out, u.String())
-		})
+func TestStringNoPath(t *testing.T) {
+	for _, test := range stringNoPathTests {
+		u, err := url.Parse(test.in)
+		require.NoError(t, err, "Parse should not return an error")
+
+		require.Equal(t, test.out, u.StringNoPath())
 	}
 }
