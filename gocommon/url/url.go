2118f7425d44c738996c38e49bf269c9297a2b91
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-03-27 22:53:46 +0000

    feat: add gocommon.url tests and functions

--- gocommon/url/url.go
+++ gocommon/url/url.go
@@ -10,10 +10,24 @@ import (
 	"net/url"
 )
 
-type URL = url.URL
+type URL struct {
+	url.URL
+}
 
 var ErrInvalidScheme = errors.New("url does not contain a valid scheme")
 var ErrNotAbsolute = errors.New("url is not absolute")
+
+func (u *URL) Parse(ref string) (*URL, error) {
+	parsed, err := u.URL.Parse(ref)
+	return &URL{URL: *parsed}, err
+}
+
+// Returns url string without path
+func (u *URL) StringNoPath() string {
+	copy := *u
+	copy.Path = ""
+	return copy.String()
+}
 
 // Parses an absolute raw url into a [URL] struct
 //
@@ -23,7 +37,7 @@ var ErrNotAbsolute = errors.New("url is not absolute")
 func ParseAbs(rawURL string) (*URL, error) {
 	u, err := Parse(rawURL)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse absolute url: %w", err)
+		return nil, err
 	}
 
 	if !u.IsAbs() {
@@ -51,5 +65,22 @@ func Parse(rawURL string) (*URL, error) {
 	u.RawFragment = ""
 	u.User = nil
 
-	return u, nil
+	return &URL{URL: *u}, nil
+}
+
+// Parses an array of strings into a [URL] array
+//
+// Uses ParseAbs
+func ParseMany(rawURLs []string) ([]*URL, error) {
+	urls := make([]*URL, 0)
+	var u *URL
+	var err error
+	for _, urlS := range rawURLs {
+		u, err = ParseAbs(urlS)
+		if err != nil {
+			return nil, err
+		}
+		urls = append(urls, u)
+	}
+	return urls, nil
 }
