40c41473c6e24831b7b209a3826af146fa41607c
Zakk <zakkcarpenter@hotmail.com>
2025-03-30 16:39:18 +0100

    create apo.go

---
+++ backend/crawlerapi/api.go
@@ -0,0 +1,42 @@
+package crawlerapi
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ZakkBob/AskDave/backend/orm"
+	"github.com/ZakkBob/AskDave/gocommon/tasks"
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func Init() {
+	router = gin.Default()
+	router.GET("/tasks", GetTasks)
+	router.POST("/results", PostResults)
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func GetTasks(c *gin.Context) {
+	t, _ := orm.NextTasks(10)
+
+	c.IndentedJSON(http.StatusOK, t)
+}
+
+func PostResults(c *gin.Context) {
+	var results tasks.Results
+
+	if err := c.BindJSON(&results); err != nil {
+		return
+	}
+
+	err := orm.SaveResults(&results)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%v")
+}
