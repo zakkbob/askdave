5703fc51a298d9091d5dba1216e687de5e2f7aa9
ZakkBob <zakkcarpenter@hotmail.com>
2025-03-30 21:23:19 +0100

    start crawlerapi in backend main.go

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/ZakkBob/AskDave/backend/crawlerapi"
 	"github.com/ZakkBob/AskDave/backend/orm"
 )
 
 func main() {
 	orm.Connect("")
+	crawlerapi.Init()
 	defer orm.Close()
 }
